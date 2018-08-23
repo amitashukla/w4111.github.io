5e0946d6f3c44768645956f4e87563f3a0c37e93
ewu <ewu@cs.columbia.edu>
2018-08-23 08:16:14 -0400

    lazy commit msg

--- advanced/submitJoinLab.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git tag -d joinsubmit
-git push origin :refs/tags/joinsubmit
-git add --all .
-git commit -a -m 'Join lab'
-git tag -a lab1submit -m 'submit join lab'
-git push origin master --tags
